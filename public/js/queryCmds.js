9663ade9547079a0edf5847fb2e3570b79079f52
saranya101 <gavarasanasrisaisaranya@gmail.com>
2024-02-14 09:12:27 +0800

    commenting

--- public/js/queryCmds.js
+++ public/js/queryCmds.js
@@ -35,72 +35,72 @@ function fetchMethod(url, callback, method = "GET", data = null, token = null) {
       .catch((error) => console.error(`Error from ${method} ${url}:`, error));
   }
 
-//=====================================================================================
-// JQUERY METHOD
-// This function uses the jQuery ajax method to make a request to the server.
-//=====================================================================================
-function jqueryMethod(url, callback, method = "GET", data = null, token = null) {
-    console.log("jqueryMethod ", url, method, data, token);
+// //=====================================================================================
+// // JQUERY METHOD
+// // This function uses the jQuery ajax method to make a request to the server.
+// //=====================================================================================
+// function jqueryMethod(url, callback, method = "GET", data = null, token = null) {
+//     console.log("jqueryMethod ", url, method, data, token);
   
-    const headers = {};
+//     const headers = {};
   
-    if (data) {
-      headers["Content-Type"] = "application/json";
-    }
+//     if (data) {
+//       headers["Content-Type"] = "application/json";
+//     }
   
-    if (token) {
-      headers["Authorization"] = "Bearer " + token;
-    }
+//     if (token) {
+//       headers["Authorization"] = "Bearer " + token;
+//     }
   
-    const jqueryConfig = {
-      url: url,
-      type: method.toUpperCase(),
-      headers: headers,
-      data: data,
-      dataType: "json",
-      success: function (responseData, textStatus, jqXHR) {
-        callback(jqXHR.status, responseData);
-      },
-      error: function (jqXHR, textStatus, errorThrown) {
-        console.error(`Error from ${method} ${url}:`, errorThrown);
-      },
-    };
+//     const jqueryConfig = {
+//       url: url,
+//       type: method.toUpperCase(),
+//       headers: headers,
+//       data: data,
+//       dataType: "json",
+//       success: function (responseData, textStatus, jqXHR) {
+//         callback(jqXHR.status, responseData);
+//       },
+//       error: function (jqXHR, textStatus, errorThrown) {
+//         console.error(`Error from ${method} ${url}:`, errorThrown);
+//       },
+//     };
   
-    $.ajax(jqueryConfig);
-  }
+//     $.ajax(jqueryConfig);
+//   }
 
-  /*  add in html
-  <script
-      src="https://code.jquery.com/jquery-3.7.0.min.js"
-      integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g="
-      crossorigin="anonymous"></script>
-      */ 
+//   /*  add in html
+//   <script
+//       src="https://code.jquery.com/jquery-3.7.0.min.js"
+//       integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g="
+//       crossorigin="anonymous"></script>
+//       */ 
 
-//=====================================================================================
-// AXIOS METHOD
-// This function uses the axios method to make a request to the server.
-//=====================================================================================
-function axiosMethod(url, callback, method = "GET", data = null, token = null) {
-    console.log("axiosMethod ", url, method, data, token);
+// //=====================================================================================
+// // AXIOS METHOD
+// // This function uses the axios method to make a request to the server.
+// //=====================================================================================
+// function axiosMethod(url, callback, method = "GET", data = null, token = null) {
+//     console.log("axiosMethod ", url, method, data, token);
   
-    const headers = {};
+//     const headers = {};
   
-    if (data) {
-      headers["Content-Type"] = "application/json";
-    }
+//     if (data) {
+//       headers["Content-Type"] = "application/json";
+//     }
   
-    if (token) {
-      headers["Authorization"] = "Bearer " + token;
-    }
+//     if (token) {
+//       headers["Authorization"] = "Bearer " + token;
+//     }
   
-    const axiosConfig = {
-      method: method.toUpperCase(),
-      url: url,
-      headers: headers,
-      data: data,
-    };
+//     const axiosConfig = {
+//       method: method.toUpperCase(),
+//       url: url,
+//       headers: headers,
+//       data: data,
+//     };
   
-    axios(axiosConfig)
-      .then((response) => callback(response.status, response.data))
-      .catch((error) => console.error(`Error from ${method} ${url}:`, error));
-  }
+//     axios(axiosConfig)
+//       .then((response) => callback(response.status, response.data))
+//       .catch((error) => console.error(`Error from ${method} ${url}:`, error));
+//   }
